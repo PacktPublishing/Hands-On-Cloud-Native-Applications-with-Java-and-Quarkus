b925eaa22aa53c39150ce282429f852812549da1
francesco <francesco@fedora.redhat.com>
2019-05-16 09:17:08 +0200

    updated chapter1 and 2

---
+++ chapter2/src/main/java/com/packt/quarkus/chapter1/SimpleRest.java
@@ -0,0 +1,27 @@
+package com.packt.quarkus.chapter1;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Path("/helloworld")
+public class SimpleRest {
+protected final Logger log = LoggerFactory.getLogger(this.getClass());
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/{name}")
+    public String hello(@PathParam("name") String name) {
+
+        log.info("Called with "+name);
+        return "hello "+name;
+    }
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello\n";
+    }
+}
---
+++ chapter2/src/test/java/com/packt/quarkus/chapter1/NativeSimpleRestIT.java
@@ -0,0 +1,9 @@
+package com.packt.quarkus.chapter1;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeSimpleRestIT extends SimpleRestTest {
+
+    // Execute the same tests but in native mode.
+}
