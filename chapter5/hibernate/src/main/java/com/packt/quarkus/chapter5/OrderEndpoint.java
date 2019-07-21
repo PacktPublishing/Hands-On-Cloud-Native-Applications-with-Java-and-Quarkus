c06b8aa40a1384d0735e9edd7cc8db7bd18c7dd4
fmarchioni <marchioni.francesco@gmail.com>
2019-07-21 11:10:54 +0200

    Update OrderEndpoint.java

--- chapter5/hibernate/src/main/java/com/packt/quarkus/chapter5/OrderEndpoint.java
+++ chapter5/hibernate/src/main/java/com/packt/quarkus/chapter5/OrderEndpoint.java
@@ -19,16 +19,12 @@ public class OrderEndpoint {
     @GET
     public List<Orders> getAll(@QueryParam("customerId") Long customerId) {
         return orderRepository.findAll(customerId);
-
     }
 
     @POST
     @Path("/{customer}")
     public Response create(Orders order, @PathParam("customer") Long customerId) {
-        System.out.println("Order is "+order);
-
         Customer c = customerRepository.findCustomerById(customerId);
-
         orderRepository.createOrder(order,c);
         return Response.status(201).build();
 
@@ -36,18 +32,12 @@ public class OrderEndpoint {
 
     @PUT
     public Response update(Orders order) {
-        System.out.println("Updating order id "+order);
         orderRepository.updateOrder(order);
         return Response.status(204).build();
     }
     @DELETE
-<<<<<<< HEAD
     @Path("/{order}")
     public Response delete(@PathParam("order") Long orderId) {
-=======
-    public Response delete(@QueryParam("id") Long orderId) {
->>>>>>> fc099194d947fb486088b902a424566c9b9fa3a7
-        System.out.println("Deleting order id "+orderId);
         orderRepository.deleteOrder(orderId);
         return Response.status(204).build();
     }
