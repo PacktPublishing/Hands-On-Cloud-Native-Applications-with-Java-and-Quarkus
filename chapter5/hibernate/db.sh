39fcb1772b931853881d3aec0aee8e46647dd316
francesco <francesco@fedora.station>
2019-07-28 16:00:50 +0200

    fixed db.sh

--- chapter5/hibernate/db.sh
+++ chapter5/hibernate/db.sh
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
+
 docker run --ulimit memlock=-1:-1 -it --rm=true --memory-swappiness=0 --name quarkus_test -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -p 5432:5432 postgres:10.5
-=======
-docker run --ulimit memlock=-1:-1 -it --rm=true --memory-swappiness=0 --name quarkus_test -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=customerdb -p 5432:5432 postgres:10.5
->>>>>>> fc099194d947fb486088b902a424566c9b9fa3a7
+
