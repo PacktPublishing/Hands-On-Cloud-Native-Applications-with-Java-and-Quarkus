2fa8b4b76fb6699831b5d7fcc9fe2180364deaae
francesco <francesco@fedora.redhat.com>
2019-11-11 11:15:40 +0100

    removed old version

--- chapter3/hello-okd/src/test/java/com/packt/quarkus/chapter3/NativeHelloOKDIT.java
+++
@@ -1,9 +0,0 @@
-package com.packt.quarkus.chapter3;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeHelloOKDIT extends HelloOKDTest {
-
-    // Execute the same tests but in native mode.
-}
