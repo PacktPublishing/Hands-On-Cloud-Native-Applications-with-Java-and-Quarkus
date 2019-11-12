2f910ed798a98a598083ef92956044b8e56ffb9e
francesco <francesco@fedora.redhat.com>
2019-11-12 15:05:39 +0100

    cr1 fix

--- chapter4/basic/customer-service/src/test/java/com/packt/quarkus/chapter4/NativeCustomerEndpointIT.java
+++
@@ -1,9 +0,0 @@
-package com.packt.quarkus.chapter4;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeCustomerEndpointIT extends CustomerEndpointTest {
-
-    // Execute the same tests but in native mode.
-}
--- chapter4/websockets/customer-service/src/test/java/com/packt/quarkus/chapter4/NativeCustomerEndpointIT.java
+++
@@ -1,9 +0,0 @@
-package com.packt.quarkus.chapter4;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeCustomerEndpointIT extends CustomerEndpointTest {
-
-    // Execute the same tests but in native mode.
-}
